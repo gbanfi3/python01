dc69cc582f90c8ab65a6d7195ed087ac8a8eb175
bg <gbanfi3@gmail.com>
2019-06-17 18:00:38 +0200

    add alma03_list

---
+++ alma03_lists.py
@@ -0,0 +1,11 @@
+# ipAddress = input("ip address: ")
+# print("ennyi pont van benne: {}".format(ipAddress.count(".")))
+# keres = input("ezt keresem: ")
+# ipAddress = "1.2.3.4.5.6.7.8.9.0"
+#
+# print("hányadik helyen van?: {}".format(ipAddress.index(keres)))
+
+parrot_list = ["egy", "ketto", "harom", "negy"]
+for i in parrot_list:
+    print("ez a madár {}".format(i))
+    print("ez a madár " + i)
