bd1f34bd34bdb1acb92aa7a6397d915c98cf44c4
bg <gbanfi3@gmail.com>
2019-06-17 18:41:13 +0200

    lists 1

--- alma03_lists.py
+++ alma03_lists.py
@@ -5,7 +5,52 @@
 #
 # print("hányadik helyen van?: {}".format(ipAddress.index(keres)))
 
-parrot_list = ["egy", "ketto", "harom", "negy"]
-for i in parrot_list:
-    print("ez a madár {}".format(i))
-    print("ez a madár " + i)
+# parrot_list = ["egy", "ketto", "harom", "negy"]
+# parrot_list.append("öt")
+#
+# for i in parrot_list:
+#     print("ez a madár {}".format(i))
+#     print("ez a madár " + i)
+
+
+# even = [2,4, 6, 8]
+# odd = [1,2,3, 4]
+#
+# all = even + odd
+# all_sorted = sorted(all)
+# all.sort()
+# print(all)
+# print(all_sorted)
+
+
+# list_1 = []
+# list_2 = list()
+# print("list 1: {}".format(list_1))
+# print("list 2: {}".format((list_2)))
+# a = list("hello world")
+# a.sort(reverse=True)
+# print(a)
+
+# even = [2, 4, 6, 8]
+
+# even2 = list(even)
+# print(even == even2)
+# odd = [1,3,5,7,9]
+# numbers = [even, odd]
+# for number_set in numbers:
+#     print(number_set)
+#
+#     for i in number_set:
+#         print(i)
+
+# menu = []
+# menu.append(["a", "b", "c"])
+# menu.append(["a", "b", "d"])
+# menu.append(["a", "e", "c"])
+# menu.append(["w", "b", "c"])
+#
+# for meal in menu:
+#     if not "a" in meal:
+#         print(meal)
+#         for i in meal:
+#             print(i)
