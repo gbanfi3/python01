d498d78edc9cddf9b9771472116d7b285cc1f3f9
gbanfi3 <gbanfi3@gmail.com>
2020-01-18 00:46:01 +0100

    dict and list collected, but not yet sorted by number

--- a5_file_list.py
+++ a5_file_list.py
@@ -1,21 +1,20 @@
 import os
 
+filenamesAllDict = {}
+filenamesAllList = []
+
 for dirname, dirnames, filenames in os.walk('/home/gbanfi3/alma'):
-    # print path to all subdirectories first.
-    # print("-- ", dirname)
-    # print(dirnames)
-    # print(filenames)
-    # for subdirname in dirnames:
-    #     print(os.path.join(dirname, subdirname))
 
     # print path to all filenames.
     for filename in filenames:
         filepath = os.path.join(dirname, filename)
-        print(filepath)
-        print(os.stat(filepath).st_size)
+        filesize = os.stat(filepath).st_size
+        filenamesAllList.append((filename, filesize, filepath))
 
-    # # Advanced usage:
-    # # editing the 'dirnames' list will stop os.walk() from recursing into there.
-    # if '.git' in dirnames:
-    #     # don't go into any .git directories.
-    #     dirnames.remove('.git')
+        if filename in filenamesAllDict.keys():
+            filenamesAllDict[filename] +=1
+        else:
+            filenamesAllDict[filename] = 1
+
+print(filenamesAllDict)
+print(filenamesAllList)
