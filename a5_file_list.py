63bbe1f9209fdbff71b32f67ef00bdebe9cd2378
gbanfi3 <gbanfi3@gmail.com>
2020-01-18 01:23:24 +0100

    sorted: number of occurrence

--- a5_file_list.py
+++ a5_file_list.py
@@ -1,11 +1,18 @@
 import os
 import operator
+from operator import itemgetter
 import pprint
+
+pp = pprint.PrettyPrinter(indent=4)
+
+#dirr = '/home/gbanfi3/alma'
+#dirr = '/media/gbanfi3/WD2T/tanulás/logika/'
+dirr = '/media/gbanfi3/WD2T/tanulás/IT/'
 
 filenamesAllDict = {}
 filenamesAllList = []
 
-for dirname, dirnames, filenames in os.walk('/home/gbanfi3/alma'):
+for dirname, dirnames, filenames in os.walk(dirr):
 
     # print path to all filenames.
     for filename in filenames:
@@ -18,10 +25,15 @@ for dirname, dirnames, filenames in os.walk('/home/gbanfi3/alma'):
         else:
             filenamesAllDict[filename] = 1
 
-sortedFilenamesAllList =sorted(filenamesAllDict.items(), key=operator.itemgetter(0))
+#pp.pprint(filenamesAllDict)
+#pp.pprint(filenamesAllList)
 
-pp = pprint.PrettyPrinter(indent=4)
-pp.pprint(filenamesAllDict)
-pp.pprint(filenamesAllList)
+#sortedFilenamesAllList =sorted(filenamesAllDict.items(), key=operator.itemgetter(0))
+#pp.pprint(sortedFilenamesAllList)
+
+sortedFilenamesAllList = list(filenamesAllDict.items())
+sortedFilenamesAllList.sort(key=itemgetter(1))
 pp.pprint(sortedFilenamesAllList)
+
+
 
