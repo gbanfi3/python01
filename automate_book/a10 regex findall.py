ca4dd4ef760b5ba39fb6a45d7f08d60e6a9e785c
gbanfi3 <gbanfi3@gmail.com>
2020-01-21 20:18:18 +0100

    a10

--- automate_book/a10 regex findall.py
+++ automate_book/a10 regex findall.py
@@ -1,4 +1,4 @@
-import re
+import re, pprint
 
 szoveg = '''
  j lkjf ljsafk léjfl sdufiosp dfjksldé jfskdl fudli fjkl dfjdkls fjk ldéjfkdal sfjslkda ziosduzt rpo578 pe ui 
@@ -12,5 +12,38 @@ szoveg = '''
 '''
 
 aRegex = re.compile(r'(\d(\d\d))-(\d\d\d)-(\d\d\d\d)')
-mo = aRegex.findall(szoveg)
+mo = aRegex.findall(szoveg) # a findall nem match objectet ad vissza, hanem egy list-et
 print(mo)
+
+# \d \D szám
+# \w \W szám, betü, _
+# \s \S space tab, newline
+
+#=================================================================
+
+aa = '''
+12 drummers drumming
+11 pipers piping
+10 lords a leaping
+9 ladies dancing
+8 maids a milking
+7 swans a swimming
+6 geese a laying
+5 gold rings, badam-pam-pam
+4 calling birds
+3 French henss
+2 turtlee doves
+And 1 partridge in a pear tree
+'''
+
+aRegex = re.compile(r'(\d+) (\D+)\n')
+lista = aRegex.findall(aa)
+pprint.pprint(lista)
+
+#=================================================================
+
+maganhangzo = re.compile(r'[^aeioúőóüöűáéaA-Z]{2}') #tagadás
+lista = maganhangzo.findall(aa)
+pprint.pprint(lista)
+
+#=================================================================
